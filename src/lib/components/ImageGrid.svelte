9a8904c5950c217780d985be46676f6528c5a4c7
mikeydoesit <msquireobeng@gmail.com>
2023-10-18 13:48:17 +0000

    testing lightbox

--- src/lib/components/ImageGrid.svelte
+++ src/lib/components/ImageGrid.svelte
@@ -1,23 +1,45 @@
 <script>
     import Posts from '../components/data/Posts'
+    import { 
+        Lightbox,
+        LightboxGallery,
+        GalleryThumbnail,
+        GalleryImage
+    } from 'svelte-lightbox'
+    
 </script>
 
 <style lang="postcss">
     .image_grid {
         @apply grid grid-cols-three_column;
     }
-    .post_wrapper {
+    .svelte-lightbox-thumbnail {
         @apply h-36;
     }
-    .post_wrapper img {
+    .svelte-lightbox-thumbnail img {
         @apply h-full w-full object-cover;
     }
 </style>
 
-<section class="image_grid">
-    {#each Posts as post}
-        <div class="post_wrapper">
-            <img src={post.url} alt={post.title} />
+
+<section class="gallery">
+<LightboxGallery>
+    <!-- Layout-->
+    <svelte:fragment slot="thumbnail">
+        <div class="image_grid">
+            {#each Posts as post}
+                <GalleryThumbnail>
+                    <img src={post.url} alt={post.title}>
+                </GalleryThumbnail>
+            {/each}
         </div>
+    </svelte:fragment>
+    
+    {#each Posts as post}
+        <GalleryImage>
+            <img src={post.url} alt={post.title}>
+        </GalleryImage>
     {/each}
+</LightboxGallery>
+
 </section>
